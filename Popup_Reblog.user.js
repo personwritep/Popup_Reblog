c8ab2e06104c8549fdce1470b78241e06f7da744
Personwritep <89448434+personwritep@users.noreply.github.com>
2025-01-15 12:04:19 +0900

    Update Popup_Reblog.user.js

--- Popup_Reblog.user.js
+++ Popup_Reblog.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Popup Reblog
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  リブログリンクを別タブで開く
 // @author       Ameba Blog User
 // @match        https://ameblo.jp/*
